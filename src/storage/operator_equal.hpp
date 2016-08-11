3eddff51f8d0d7fd9560fbf394c4a322b70ba542
Morten V. Pedersen <morten@mortenvp.com>
2016-08-11 21:08:57 +0200

    Working on it

--- src/storage/operator_equal.hpp
+++ src/storage/operator_equal.hpp
@@ -9,16 +9,7 @@
 
 namespace storage
 {
-    /// Compares two storage objects to see whether they are
-    /// equal. The condition for two storage objects are bit wider
-    /// than for is_same. Two storage objects are equal either if the
-    /// point to exactly the same storage or if they contain exactly
-    /// the same data.
-    ///
-    /// @param a The first storage object
-    /// @param b The second storage object
-    /// @return True if the storage objects contain the same data
-    ///         otherwise false.
+    /// @copydoc is_equal(const const_storage&, const const_storage&)
     inline bool operator==(const const_storage& a, const const_storage& b)
     {
         return is_equal(a, b);
