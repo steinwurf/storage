517f1c4e4fb620759f45a2ebe0bee45c48dd75f9
Jeppe Pihl <jpihl08@gmail.com>
2018-01-03 13:58:02 +0100

    improve operator_equal test

--- test/src/test_operator_equal.cpp
+++ test/src/test_operator_equal.cpp
@@ -15,9 +15,12 @@ TEST(test_operator_equal, equal)
 {
     std::vector<uint8_t> d1(10, 1);
     std::vector<uint8_t> d2(10, 2);
-    std::vector<uint8_t> d3(15, 3);
+    std::vector<uint8_t> d3(15, 1);
+    std::vector<uint8_t> d3_2(15, 1);
 
     EXPECT_EQ(storage::storage(d1), storage::storage(d1));
     EXPECT_NE(storage::storage(d1), storage::storage(d2));
     EXPECT_NE(storage::storage(d1), storage::storage(d3));
+    EXPECT_NE(storage::storage(d1), storage::storage(d3_2));
+    EXPECT_EQ(storage::storage(d3), storage::storage(d3_2));
 }
